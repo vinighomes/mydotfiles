2633b8da6979e321bfe80c0daaa9d3331720c224
vinighomes <viniciusghomes15@hotmail.com>
2024-03-23 13:52:53 -0300

    first

---
+++ .local/share/Trash/files/dotfiles-master/.config/awesome/autostart.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+#run "megasync"
+run "xscreensaver -no-splash"
+#run "/usr/bin/dropbox"
+#run "insync start"
+run "picom"
+#run "/usr/bin/redshift"
+run "mpd"
+run "nm-applet"
